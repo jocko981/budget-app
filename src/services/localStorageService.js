0c446a8eaf2321f116e075b4d40a8360c4eaa8ec
jocko981 <jovannikolic17@gmail.com>
2022-04-07 00:26:46 +0200

    local storage functions

--- src/services/localStorageService.js
+++ src/services/localStorageService.js
@@ -3,8 +3,21 @@ export const ITEM_NAMES = {
     listOfExpenses: "listOfExpenses"
 }
 
-export const setLocalStorateItem = (itemName, itemValue) => { }
+export const setLocalStorateItem = (itemName, itemValue) => {
+    const item = JSON.stringify(itemValue)
+    if (typeof item !== "undefined" && item !== "null" && item !== "[object Object]") {
+        localStorage.setItem(itemName, item)
+    }
+}
 
-export const getLocalStorateItem = (itemName) => { }
+export const getLocalStorateItem = (itemName) => {
+    const item = JSON.parse(localStorage.getItem(itemName))
 
-export const updateLocalStorateItem = (itemName) => { }
+    return item;
+}
+
+export const removeLocalStorateItem = (itemName) => { }
+
+// helpers
+export const setListOfIncome = () => { }
+export const setListOfExpenses = () => { }
